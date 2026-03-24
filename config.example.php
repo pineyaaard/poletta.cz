639a99a0684f4a38c937779b144f7035dbd61825
Pavel Dmitrevskij <pineyaaard@gmail.com>
2026-03-24 22:30:50 +0100

    Delete config.example.php

--- config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ПРИМЕР КОНФИГА. НА СЕРВЕРЕ (FTP) ПЕРЕИМЕНУЙ ЭТОТ ФАЙЛ В config.php 
-// И ВПИШИ СЮДА СВОЙ РЕАЛЬНЫЙ ПАРОЛЬ!
-
-define('ADMIN_PASSWORD', 'YOUR_SECURE_PASSWORD_HERE');
-define('ADMIN_EMAIL', 'order@poletta.cz');
-define('LEADS_FILE', 'poletta_leads_99x.csv');
-?>
